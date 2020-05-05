78676c76908f4ab668ed8ba0a4044f771f8af4da
Aless <Aless@Alessio-PC.station>
2020-05-05 10:33:46 +0200

    aggiunta variabile nome

--- DocenzaGIT/src/ProvaGIT.java
+++ DocenzaGIT/src/ProvaGIT.java
@@ -3,7 +3,10 @@ public class ProvaGIT {
 
 	public static void main(String[] args) {
 
+		String nome = "Alessio";
+		
     System.out.println("Hello World!");
+    System.out.println(nome);
 
 	}
 
